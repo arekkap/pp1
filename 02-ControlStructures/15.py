aa152507009ae5d92b812033ce0868ffdf6bdb8f
s-115-25 <s-115-25@gumiforest.ae.krakow.pl>
2019-10-21 12:59:46 +0200

    Exercise

---
+++ 02-ControlStructures/15.py
@@ -0,0 +1,3 @@
+x=int(input('Podaj liczbe: '))
+for i in range(1,11):
+    print(f'{x}*{i}={x*i}')
---
+++ 02-ControlStructures/22.py
@@ -0,0 +1,8 @@
+tab = [15, 8, 31, 47, 2, 19]
+suma=0
+ilosc=len(tab)
+for i in tab:
+    suma+=i
+    
+srednia=suma/ilosc
+print(f'Średnia wynosi: {srednia}')
